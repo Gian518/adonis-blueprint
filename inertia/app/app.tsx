13c8aed63292f690219ec434e410d46e2f0eefc0
Gian518 <gian518@me.com>
2025-07-27 19:40:27 +0200

    Ant Design setup

--- inertia/app/app.tsx
+++ inertia/app/app.tsx
@@ -5,6 +5,7 @@ import '../css/app.css'
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
+import '@ant-design/v5-patch-for-react-19'
 
 const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
 
