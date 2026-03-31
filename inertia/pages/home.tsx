994c4e289b3d6300be4c171e66bb1488928c2eb0
Gian518 <gian518@me.com>
2026-04-01 00:10:02 +0200

    OneSignal missing parts alignment

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,17 +1,12 @@
 import { Head } from '@inertiajs/react'
-import { Button } from 'antd'
-import OneSignal from 'react-onesignal'
+import OneSignalAlert from '~/components/onesignal-alert'
 
 export default function Home() {
-	const promptOneSignal = async () => {
-		await OneSignal.Notifications.requestPermission()
-	}
-
 	return (
 		<>
 			<Head title='Homepage' />
 
-			<Button onClick={() => promptOneSignal()} type='primary'>Prompt OneSignal</Button>
+			<OneSignalAlert />
 		</>
 	)
 }
